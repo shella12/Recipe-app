44be3923ffa187acecc62f1d63b1a3bd354d1d6c
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-06 11:40:13 +0430

    Add foods controller with thier methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  get 'foods/show'
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
