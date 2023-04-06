cd3592a380e4f2fc62f4899e3b4636e378219ee2
shella12 <ayeshaarshad4567@gmail.com>
2023-04-06 10:11:32 +0500

    Fix database

--- db/migrate/20230405092923_add_devise_to_users.rb
+++ db/migrate/20230405092923_add_devise_to_users.rb
@@ -3,17 +3,19 @@
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
+
+      null    = false
+    default = ""
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
+      add_column :users, :email, :string, :null => null, :default => default
+      add_column :users, :encrypted_password, :string, :null => null, :default => default, :limit => 128
+      add_column :users, :password_salt, :string
+      add_column :users, :authentication_token, :string
+      add_column :users, :reset_password_token, :string
+      add_column :users, :remember_token,      :string
+      add_column :users, :remember_created_at, :datetime
+      
+  
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
@@ -21,11 +23,21 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
 
+      add_column :users, :sign_in_count,      :integer, :default => 0
+      add_column :users, :current_sign_in_at, :datetime
+      add_column :users, :last_sign_in_at,    :datetime
+      add_column :users, :current_sign_in_ip, :string
+      add_column :users, :last_sign_in_ip,    :string
+
       # Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
       # t.datetime :confirmation_sent_at
       # t.string   :unconfirmed_email # Only if using reconfirmable
+      add_column :users, :confirmation_token,   :string
+      add_column :users, :confirmed_at,         :datetime
+      add_column :users, :confirmation_sent_at, :datetime
+      
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -39,7 +51,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -52,10 +52,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_092923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password", limit: 128, default: "", null: false
+    t.string "password_salt"
+    t.string "authentication_token"
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "remember_token"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
