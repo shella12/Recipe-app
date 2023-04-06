53807f373194c89279726244b3d67c4834b511d0
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-06 14:17:07 +0430

    Add data to user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create([{name: "Zuhra"}])
+second_user = User.create([{name: "Hashimi"}])
