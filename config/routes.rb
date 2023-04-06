f75e431b873af11907d0cf67c7fccdc212446465
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-06 14:16:43 +0430

    Add the food path

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'foods/index'
-  get 'foods/show'
-  get 'foods/new'
-  get 'foods/create'
-  get 'foods/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :show, :new, :create]
+  end
 end
