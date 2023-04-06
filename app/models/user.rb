345c5d61b5206d8a3d0fe5c1b7b2fe57f75d2749
shella12 <ayeshaarshad4567@gmail.com>
2023-04-06 10:57:24 +0500

    Add sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :trackable
+        :trackable
   has_many :recipes, dependent: :destroy
   has_many :foods, dependent: :destroy
 end
