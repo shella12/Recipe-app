0922ffd8ddb301f902cfba8ea1d40ff86e133e3c
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-07 12:35:27 +0430

    Refactor the relation between tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :foods, foreign_key: :user_id, dependent: :destroy
+  has_many :recipes, foreign_key: :user_id, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
-  has_many :recipes, dependent: :destroy
-  has_many :foods, dependent: :destroy
+  # has_many :recipes, dependent: :destroy
+  # has_many :foods, dependent: :destroy
+
 end
