a2affaa08e66ea04d1c673ca15a576b6e6ed193e
shella12 <ayeshaarshad4567@gmail.com>
2023-04-08 22:59:32 +0500

    Fix linter errors

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-    factory :recipe do
-      name { Faker::Food.dish }
-      description { Faker::Food.description }
-      preparationTime { Faker::Number.between(from: 1, to: 60) }
-      cookingTime { Faker::Number.between(from: 1, to: 60) }
-      user
-    end
+  factory :recipe do
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    preparationTime { Faker::Number.between(from: 1, to: 60) }
+    cookingTime { Faker::Number.between(from: 1, to: 60) }
+    user
   end
+end
