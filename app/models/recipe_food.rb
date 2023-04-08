42097ce710250fec6a569f7da1467af1abf21b8c
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-04-08 16:53:35 +0430

    Add controller for shoping list

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,6 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :food
-  belongs_to :recipe
+  belongs_to :recipe, foreign_key: 'recipe_id'
+  belongs_to :food, foreign_key: 'food_id'
+
+  validates :quantity, presence: true
 end
