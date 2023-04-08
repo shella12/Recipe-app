93ad442d2b64583c524b96759040c3494c3cd645
shella12 <ayeshaarshad4567@gmail.com>
2023-04-08 21:26:31 +0500

    Add all tests

--- spec/factories/recipe.rb
+++ spec/factories/recipe.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
-    factory :recipe do
-      user
-      name { Faker::Food.dish }
-      preparationTime {Faker::Number.decimal(l_digits: 2) }
-      cookingTime { Faker::Number.decimal(l_digits: 2) }
-      description {Faker::Food.description}
-    end
+  factory :recipe do
+    user
+    name { Faker::Food.dish }
+    preparationTime { Faker::Number.decimal(l_digits: 2) }
+    cookingTime { Faker::Number.decimal(l_digits: 2) }
+    description { Faker::Food.description }
+    public { true }
   end
+end
--- spec/views/recipes/index.html.erb_spec.rb
+++ spec/views/recipes/index.html.erb_spec.rb
@@ -3,28 +3,19 @@ require 'rails_helper'
 RSpec.feature 'RecipeList', type: :system do
   let(:user) { FactoryBot.create(:user) }
   let(:recipe) { FactoryBot.create(:recipe, user:) }
-
   before do
-    # user.confirm # Confirm the user's email address
-    login_as(user) # sign in the user
+    login_as(user)
+    recipe.save
   end
 
   describe 'index page' do
     it 'shows the right content' do
-      visit('/users/1/posts')
-      expect(page).to have_selector('img.profile_photo')
-      expect(page).to have_selector('h1', text: @user.name)
-      expect(page).to have_selector('p', text: "Number of posts: #{@user.posts_counter}")
-      @posts.each do |post|
-        expect(page).to have_selector('h2', text: post.title.strip)
-        expect(page).to have_selector('p', text: post.text)
-        expect(page).to have_content(post.comments.first.text) unless post.comments.first.nil?
-        expect(page).to have_content("likes: #{post.likes_counter}")
-        expect(page).to have_content("comments: #{post.comments_counter}")
-      end
-      click_link("Post##{@posts.first.id}")
-      sleep(1)
-      expect(current_path).to eql("/users/#{@user.id}/posts/#{@posts.first.id}")
+      visit('/recipes')
+      expect(page).to have_selector('a', text: 'Recipe 1')
+      expect(page).to have_selector('p', text: recipe.description.to_s)
+      click_link('Remove')
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content('Recipe has been deleted successfully!')
     end
   end
 end
